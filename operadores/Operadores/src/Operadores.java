cdcbc3feb9327eabfc89d6d4fc0a149104a234bf
lukeskywalkerZ <93051844+lukeskywalkerZ@users.noreply.github.com>
2022-10-23 16:25:04 -0300

    23-10

---
+++ operadores/Operadores/src/Operadores.java
@@ -0,0 +1,47 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+       //Operadores
+       
+       //Atribuição =
+       String nome = "JOSÉ";
+       char sexo = 'M';
+       //Date datanasc = new Date();
+
+       //Aritméticos
+       double soma = 10.5 + 15.7;
+       int subtração = 112 - 20;
+       int divisao = 15 / 3;
+       int modulo = 18 % 3;
+
+       // + texto = CONCATENA
+
+       String nomeCompleto = "LINGUAGEM" + " JAVA";
+
+       System.out.println(nomeCompleto);
+
+       //Unários - Juntamente com operador aritmético
+            // Incrementar, decrementar, inverter valores e booleanos
+       // + - ! - Negação
+
+       //x repetição
+
+       int numero = 5;
+
+       //numero = numero + 1;
+
+       numero++;
+
+       System.out.println(numero);
+
+       //booleano
+
+       boolean variavel = true;
+
+       variavel = !variavel;
+
+       //Operador Ternário - Forma resumida de definir condição
+       //Escolher entre 2 valores - Como se fosse IF - Única
+
+
+    }
+}
--- tipos-variaveis-operadores/tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis-operadores/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //int, byte, short, long, float, double, boolean e char
+        
+        // variaveis
+        byte idade = 123;
+        short anoFabricacao = 2021;
+        int cep = 15000365;
+        long cpf = 12345678911L;
+        float pi = 3.14F;
+        double salario = 2500;
+
+        String meuNome = "Julio";
+
+        // constantes
+        final double VALOR_DE_PI = 3.14; // CAIXA ALTA
     }
 }
