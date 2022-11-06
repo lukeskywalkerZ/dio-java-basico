c222adaca0456b5844cfdaed98bad513c747f373
lukeskywalkerZ <93051844+lukeskywalkerZ@users.noreply.github.com>
2022-11-06 14:55:47 -0300

    06-11

---
+++ similaridade/src/Similaridade.java
@@ -0,0 +1,12 @@
+public class Similaridade {
+   
+    public static void main(String[] args) throws Exception {
+        int n1 = 10;
+        int n2 = 20;
+    
+        String s1 = "Nome";
+        String s2 = "Sobrenome";
+    
+        System.out.println("n1 == n2 " + (n1 == n2));
+    }
+}
